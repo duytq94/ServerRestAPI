77b3137356775e1890789e208ffb9878ae01861c
duytq <duy.tran@beesightsoft.com>
2017-10-06 03:54:50 -0700

    update

--- Models/todo.js
+++ Models/todo.js
@@ -1,40 +1,70 @@
 var connection = require('../connection');
   
 function Todo() {
-  this.get = function(groupId, page, pageSize, res) {
-    connection.acquire(function(err, con) {
+  this.getHistory = function(groupId, page, pageSize, res) {
+    connection.acquire(function(err, conn) {
 
     	page = parseInt(page) - 1;
     	pageSize = parseInt(pageSize);
 
-      con.query('SELECT * FROM (SELECT * FROM archive WHERE to_group = ? ORDER BY id DESC LIMIT ?, ?) sub ORDER BY id ASC', [groupId, page * pageSize, pageSize], function(err, result) {
-        con.release();
-     
-        if (!err) {
-        	 res.json(
-                {
-                    'data': result,
-                    'error': false,
-                    'errors': null
-                }
-            );
-   
-        } else {
-        	res.json(
-                {
-                    'data': null,
-                    'error': true,
-                    'errors': [{
-                        'errorCode': 9011,
-                        'errorMessage': 'Please make sure your request has an Authorization header'
-                    }]
-                }
-            );
-            throw err;
-		}
-        // res.send(result);
+        conn.query('SELECT * FROM (SELECT * FROM archive WHERE to_group = ? ORDER BY id DESC LIMIT ?, ?) sub ORDER BY id ASC', [groupId, page * pageSize, pageSize], function(err, result) {
+            conn.release();
+         
+            if (!err) {
+            	 res.json(
+                    {
+                        'data': result,
+                        'error': false,
+                        'errors': null
+                    }
+                );
+       
+            } else {
+            	res.json(
+                    {
+                        'data': null,
+                        'error': true,
+                        'errors': [{
+                            'errorCode': 9011,
+                            'errorMessage': 'Something error'
+                        }]
+                    }
+                );
+                throw err;
+    		}
       });
     });
   };
+
+  this.getStatus = function(groupId, res) {
+    connection.acquire(function(err, conn) {
+        conn.query('SELECT * FROM users WHERE to_group = ? ', [groupId], function(err, result) {
+            conn.release();
+            if (!err) {
+                 res.json(
+                    {
+                        'data': result,
+                        'error': false,
+                        'errors': null
+                    }
+                );
+       
+            } else {
+                res.json(
+                    {
+                        'data': null,
+                        'error': true,
+                        'errors': [{
+                            'errorCode': 9011,
+                            'errorMessage': 'Something error'
+                        }]
+                    }
+                );
+                throw err;
+            }
+        })
+    })
+  }
+
 }
 module.exports = new Todo();
--- routes.js
+++ routes.js
@@ -2,8 +2,14 @@ var todo = require('./Models/todo');
   
 module.exports = {
   configure: function(app) {
+
     app.get('/history/:groupId', function(req, res) {
-          todo.get(req.params.groupId, req.query.page, req.query.pageSize, res);
+        todo.getHistory(req.params.groupId, req.query.page, req.query.pageSize, res);
     });
+
+    app.get('/status/:groupId', function(req, res) {
+    	todo.getStatus(req.params.groupId, res);
+    })
+
   }
 };	
